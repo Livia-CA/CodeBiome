b6ba03da508c471c83bbe648fdfc2b528f424b4d
Livia-CA <liviacaetanodeandrade@gmail.com>
2023-08-12 08:44:51 -0400

    adiciona tela sobre e tela do jogo

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ function closed_nivels(){
 function open_nivels(){
     document.getElementById("fases").className="closed";
 }
+
+function close_modal(id){
+    document.getElementById(id).className="invisible";
+}
+
+function open_modal(id){
+    document.getElementById(id).className="visible";
+}
