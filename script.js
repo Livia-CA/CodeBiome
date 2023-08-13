cf14feadaf5d06dd4b727c001af405f16c0d6342
Livia Caetano de Andrade <liviacaetanodeandrade@gmail.com>
2023-08-13 16:53:09 -0400

    adiciona código js

--- script.js
+++ script.js
@@ -24,11 +24,122 @@ function open_modal(id){
     document.getElementById(id).className="visible";
 }
 
-function teste(){
+function loading(){
     document.getElementById("loading").className="visible";
 
     setTimeout(() => {
         document.getElementById("loading").className="invisible";
+        document.getElementById("jogo").className="visible";
     }, "3000");
 
+}
+
+var nivel_atual = 1;
+var nivel_desbloc = 3;
+
+function atualiza_mudar_nivel(){
+    if(nivel_atual == nivel_desbloc){
+        var elements = document.getElementsByClassName("level_seg");
+    
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].classList.add("block");
+        }
+    }else{
+        var elements = document.getElementsByClassName("level_seg");
+    
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].classList.remove("block");
+        }
+    }
+    if(nivel_atual == 1){
+        var elements = document.getElementsByClassName("level_ant");
+    
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].classList.add("block");
+        }
+    }else{
+        var elements = document.getElementsByClassName("level_ant");
+    
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].classList.remove("block");
+        }
+    }    
+}
+
+function mudarnivel(element){
+    close_modal(`nivel${nivel_atual}`);
+    if(element.classList.contains("level_ant")){
+        nivel_atual--;
+    }else{
+        nivel_atual++;
+    }
+    open_modal(`nivel${nivel_atual}`)
+    atualiza_mudar_nivel();
+}
+
+document.addEventListener("keydown", (event)=>{
+    var resp_user;
+    var nivel;
+    if(document.getElementById('jogo').className == "visible"){
+        if(event.key === ";"){
+            if(document.getElementById('nivel1').className == "visible"){
+                resp_user = document.getElementById("resp_nivel1").value;
+                nivel = 1;
+            }
+            if(document.getElementById('nivel2').className == "visible"){
+                resp_user = document.getElementById("resp_nivel2").value;
+                nivel = 2;
+            }
+            if(document.getElementById('nivel3').className == "visible"){
+                resp_user = document.getElementById("resp_nivel3").value;
+                nivel = 3;
+            }
+    
+            resp_user = resp_user.toLowerCase();
+            resp_user = resp_user.replace(/\s/g,'');
+
+            atualizaCSS(resp_user, nivel);
+        }
+    }
+})
+
+function atualizaCSS(texto, nivel){
+    switch (texto){
+        case "justify-content:flex-end":
+            document.getElementById(`campo_nivel${nivel}`).style.justifyContent = "flex-end";
+            break;
+
+        case "justify-content:space-between":
+            campo.style.justifyContent = "space-between";
+            break;
+
+        default:
+            alert("deu certo não, tenta ai novamente");
+            alert(texto);
+            break;
+    }
+}
+
+function mudar_instrucao(){
+
+    var title_story = document.getElementById(`t_hist${nivel_atual}`);
+    var title_flex = document.getElementById(`t_flex${nivel_atual}`);
+
+    var story = document.getElementById(`hist${nivel_atual}`);
+    var expla = document.getElementById(`expl${nivel_atual}`);
+    
+    if(title_story.classList.contains("active")){
+        title_story.classList.remove("active");
+        title_flex.classList.add("active");
+
+        story.classList.add("invisible");
+        expla.classList.remove("invisible");
+    }else{
+        title_story.classList.add("active");
+        title_flex.classList.remove("active");
+
+        story.classList.remove("invisible");
+        expla.classList.add("invisible");
+    }
+    
 }
